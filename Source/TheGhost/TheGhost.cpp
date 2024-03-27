6007b1f01c3ccd4668e41473e499fb8e0fc838b9
objectio <objection@kakao.com>
2024-03-27 18:52:31 +0900

    [ADD] Initial

---
+++ Source/TheGhost/TheGhost.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheGhost.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheGhost, "TheGhost" );
---
+++ Source/TheGhost/TheGhostGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheGhostGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGHOST_API ATheGhostGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
