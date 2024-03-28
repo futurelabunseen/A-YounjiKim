17cde464444c26f49c00941481222390337d84e3
objectio <objection@kakao.com>
2024-03-28 21:18:52 +0900

    [FEAT] Including header & Implement GameMode Source

---
+++ Source/TheGhost/Game/TGGameMode.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/TGGameMode.h"
+#include "TGGameMode.h"
+
+ATGGameMode::ATGGameMode()
+{
+	// DefaultPawnClass - 
+	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Script/TheGhost.TGCharacterPlayer"));
+	if (ThirdPersonClassRef.Class)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}
+	// PlayerControllerClass - PlayerController에 대한 설정
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerClassRef(TEXT("/Script/TheGhost.TGPlayerController"));
+	if (PlayerControllerClassRef.Class) {
+		PlayerControllerClass = PlayerControllerClassRef.Class;
+	}
+}
