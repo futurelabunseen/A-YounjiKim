f407eda215cc77db80749ebd45c9ce18889ff48f
objectio <objection@kakao.com>
2024-03-28 21:20:28 +0900

    [FEAT] Implement Character & Camera Basic Moving

---
+++ Source/TheGhost/Player/TGPlayerController.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/TGPlayerController.h"
+
+void ATGPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 시작하자마자 뷰포트 안으로 포커스 들어가기
+	FInputModeGameOnly GameOnlyInputMode;
+	SetInputMode(GameOnlyInputMode);
+}
