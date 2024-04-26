7a5ccf6a53044b116be7a3ae23c5bcb49da55ba4
objectio <objection@kakao.com>
2024-04-26 11:35:31 +0900

    [FEAT] Add macros about Collision ... Capsule, Trigger, and Trace Channel

--- Source/TheGhost/Physics/TGCollision.h
+++ Source/TheGhost/Physics/TGCollision.h
@@ -5,5 +5,5 @@
 
 #define CPROFILE_TGCAPSULE TEXT("TGCapsule") // Character Capsule Profile (Preset) 을 앞으로 CPROFIILE_TGCAPSULE로 지칭
 #define CPROFILE_TGTRIGGER TEXT("TGTrigger") // Trigger Profile (Preset)
-#define CCHANNEL_ABACTION ECC_GameTraceChannel1 // Trace Channel
+#define CCHANNEL_TGACTION ECC_GameTraceChannel1 // Trace Channel
 // 위 셋은 ini 파일에서 지정된 채널 열거형 값으로 변환된다.
